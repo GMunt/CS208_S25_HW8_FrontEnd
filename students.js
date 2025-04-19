26c22bd4cca4785981e2417382accb1768510fed
Gage Munt <gagemunt@u.boisestate.edu>
2025-04-19 02:50:28 -0600

    Implemented front-end POST /students

--- students.js
+++ students.js
@@ -1,9 +1,9 @@
 console.log('students.js is executing...');
 
-// const id_form_create_new_student = document.getElementById("id_form_create_new_student");
-// id_form_create_new_student.addEventListener('submit', handleCreateNewStudentEvent);
+const id_form_create_new_student = document.getElementById("id_form_create_new_student");
+id_form_create_new_student.addEventListener('submit', handleCreateNewStudentEvent);
 
-// const div_create_new_student = document.getElementById("create_new_student");
+const div_create_new_student = document.getElementById("create_new_student");
 const div_show_student_details = document.getElementById("show_student_details");
 // const div_update_student_details = document.getElementById("update_student_details");
 // const div_delete_student = document.getElementById("delete_student");
@@ -107,9 +107,61 @@ async function getStudent(studentId)
     return null;
 }
 
+async function createNewStudent(studentData)
+{
+    const API_URL = 'http://localhost:8080/students';
+
+    try
+    {
+        const response = await fetch(API_URL, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded"
+            },
+            body: new URLSearchParams(studentData)
+        });
+        console.log({response});
+        console.log(`response.status = ${response.status}`);
+        console.log(`response.statusText = ${response.statusText}`);
+        console.log(`response.ok = ${response.ok}`);
+
+        if (response.ok)
+        {
+            const createdStudent = await response.json();
+            div_create_new_student.innerHTML = `<p class="success">Student created successfully. The new student id is ${createdStudent.id}</p>`;
+            await getAndDisplayAllStudents();
+        }
+        else
+        {
+            div_create_new_student.innerHTML = '<p class="failure">ERROR: failed to create the new student.</p>';
+        }
+    }
+    catch(error)
+    {
+        console.log(error);
+        div_create_new_student.innerHTML = '<p class="failure">ERROR: failed to connect to the API to create the new student.</p>';
+    }
+}
+
 // =====================================================================================================================
 // Functions that update the HTML by manipulating the DOM
 // =====================================================================================================================
+
+
+async function handleCreateNewStudentEvent(event)
+{
+    event.preventDefault();
+
+    const formData = new FormData(id_form_create_new_student);
+    const studentData =
+        {
+            firstName: formData.get('firstName'),
+            lastName: formData.get('lastName'),
+            birthDate: formData.get('birthDate')
+        };
+    console.log({studentData});
+    await createNewStudent(studentData);
+}
 
 function displayStudents(listOfStudentsAsJSON) {
     div_list_of_students.innerHTML = '';
