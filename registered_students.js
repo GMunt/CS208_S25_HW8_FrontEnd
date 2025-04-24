139f0786526710df1be1ed156f77d723d03bc282
Gage Munt <gagemunt@u.boisestate.edu>
2025-04-24 00:21:40 -0600

    Implemented front-end GET /registered_students

--- registered_students.js
+++ registered_students.js
@@ -1,6 +1,13 @@
 console.log('registered_students.js is executing...');
 
-addEventListener('DOMContentLoaded', getAllClassesAndRefreshTheSelectClassForEnrollmentDropdown);
+const div_select_class_for_enrollment= document.getElementById('selectClassForEnrollment');
+const div_list_of_registered_students= document.getElementById('list_of_registered_students');
+
+document.addEventListener('DOMContentLoaded', async function()
+{
+   await getAllClassesAndRefreshTheSelectClassForEnrollmentDropdown();
+   await getAllAndDisplayRegisteredStudentsAndRefreshTheSelectStudentForEnrollmentDropdown();
+});
 
 async function getAllClassesAndRefreshTheSelectClassForEnrollmentDropdown()
 {
@@ -26,17 +33,68 @@ async function getAllClassesAndRefreshTheSelectClassForEnrollmentDropdown()
         else
         {
             // TODO: update the HTML with information that we failed to retrieve the classes
+            div_select_class_for_enrollment.innerHTML = '<p class="failure">ERROR: failed to retrieve the classes.</p>';
         }
     }
     catch (error)
     {
         console.error(error);
         // TODO: update the HTML with information that we failed to connect to the API to fetch the classes data
+        div_select_class_for_enrollment.innerHTML = '<p class="failure">ERROR: failed to connect to the API to fetch the classes data.</p>';
     }
 
     console.log('getAllClassesAndRefreshTheSelectClassForEnrollmentDropdown - END');
 }
 
+async function getAllAndDisplayRegisteredStudentsAndRefreshTheSelectStudentForEnrollmentDropdown()
+{
+    console.log('getAllAndDisplayRegisteredStudentsAndRefreshTheSelectStudentForEnrollmentDropdown - START');
+    const API_URL = "http://localhost:8080/registered_students";
+
+    try
+    {
+        const response = await fetch(API_URL);
+        console.log({response});
+        console.log(`response.status = ${response.status}`);
+        console.log(`response.statusText = ${response.statusText}`);
+        console.log(`response.ok = ${response.ok}`);
+
+        if (response.ok)
+        {
+            const listOfRegisteredStudentsAsJSON = await response.json();
+            console.log({listOfRegisteredStudentsAsJSON});
+            displayRegisteredStudents(listOfRegisteredStudentsAsJSON);
+            refreshTheListOfRegisteredStudents(listOfRegisteredStudentsAsJSON);
+        }
+        else
+        {
+            div_list_of_registered_students.innerHTML = '<p class="failure">ERROR: failed to retrieve the registered students.</p>';
+        }
+    }
+    catch (error)
+    {
+        console.error(error);
+        div_list_of_registered_students.innerHTML = '<p class="failure">ERROR: failed to connect to the API to fetch the registered students data.</p>';
+    }
+    console.log('getAllAndDisplayRegisteredStudentsAndRefreshTheSelectStudentForEnrollmentDropdown - STOP');
+}
+
+function displayRegisteredStudents(listOfRegisteredStudentsAsJSON) {
+    div_list_of_registered_students.innerHTML = '';
+
+    for (const studentAsJSON of listOfRegisteredStudentsAsJSON)
+    {
+        console.log({studentAsJSON});
+        div_list_of_registered_students.innerHTML += renderStudentAsHTML(studentAsJSON);
+    }
+}
+function renderStudentAsHTML(studentAsJSON) {
+    return `
+        <div class="show-student-in-list" data-id="${studentAsJSON.id}">
+            <p>Name: ${studentAsJSON.studentFullName}</p>
+            <p>Registered in: ${studentAsJSON.code}</p>
+        </div>`;
+}
 
 function refreshTheSelectClassForEnrollmentDropdown(listOfClassesAsJSON)
 {
@@ -62,5 +120,30 @@ function refreshTheSelectClassForEnrollmentDropdown(listOfClassesAsJSON)
         option.text = classAsJSON.code + ": " + classAsJSON.title;  // this is the value the user chooses from the dropdown
 
         selectClassForEnrollment.appendChild(option);
+    }
+}
+function refreshTheListOfRegisteredStudents(listOfRegisteredStudentsAsJSON)
+{
+    const selectStudentForEnrollment = document.getElementById("selectStudentForEnrollment");
+
+    while (selectStudentForEnrollment.firstChild)
+    {
+        selectStudentForEnrollment.removeChild(selectStudentForEnrollment.firstChild);
+    }
+
+    const option = document.createElement("option");
+    option.value = "";
+    option.text = "Select a student for a class";
+    option.disabled = true;
+    option.selected = true;
+    selectStudentForEnrollment.appendChild(option);
+
+    for (const studentAsJSON of listOfRegisteredStudentsAsJSON)
+    {
+        const option = document.createElement("option");
+        option.value = studentAsJSON.id;
+        option.text = studentAsJSON.studentFullName;
+
+        selectStudentForEnrollment.appendChild(option);
     }
 }
